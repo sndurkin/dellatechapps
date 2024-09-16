a3a201c86f9a9515bfe3ee84c0e4255af156113d
Sean Durkin <sndurkin@gmail.com>
2024-09-15 22:32:47 -0400

    web: add robots.txt

--- server/config/settings.py
+++ server/config/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [ BASE_DIR / "static_files" ]
 STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
