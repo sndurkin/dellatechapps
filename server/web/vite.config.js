9d67d2aad5e42b9be8089dec7b182aa3414e768e
Sean Durkin <sndurkin@gmail.com>
2025-09-02 17:59:49 -0400

    fix bug in vite config for dev

--- server/web/vite.config.js
+++ server/web/vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: '.',
-  base: '/static/',
+  base: process.env.NODE_ENV === 'development' ? '/' : '/static/',
   build: {
     outDir: '../static/',
     assetsDir: 'assets',
